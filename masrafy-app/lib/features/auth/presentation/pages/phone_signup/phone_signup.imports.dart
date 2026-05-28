985d7d2604ea001f3dea670e98f4bd6ccc285bfd
mohammmed mokhtar <mohammedmokhtar932@gmail.com>
2026-05-28 18:20:29 +0300

    chore: update Android and iOS project configurations for compatibility with latest dependencies

--- masrafy-app/lib/features/auth/presentation/pages/phone_signup/phone_signup.imports.dart
+++ masrafy-app/lib/features/auth/presentation/pages/phone_signup/phone_signup.imports.dart
@@ -1,10 +1,13 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../../core/di/injection.dart';
 import '../../../../../core/extentions/context_locale_extention.dart';
 import '../../../../../l10n/generated/app_localizations.dart';
 import 'cubit/phone_signup/phone_signup_cubit.dart';
 
+part 'phone_signup_page.dart';
 part 'phone_signup_mobile_page.dart';
 part 'phone_signup_otp_page.dart';
 part 'phone_signup_profile_page.dart';
